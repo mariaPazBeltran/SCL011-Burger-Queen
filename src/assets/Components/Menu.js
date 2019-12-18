4d099069ed2a67154f10256ff6092c662e03980c
María Beltrán <mariabeltra.98@gmail.com>
2019-12-18 10:07:11 -0300

    menu running

--- src/assets/Components/Menu.js
+++ src/assets/Components/Menu.js
@@ -1,24 +1,41 @@
 import React, {Component} from 'react';
 import './Menu.css'
-import products from  '../data/products.json'
 
-console.log(products);
+/*products: products.Breakfast,
+      products1: products.Lunch
+      
+      {this.state.products1.map(e=><button ><img src={e.img}/><p>{e.price}</p></button>)}*/
+//console.log(products);
 class Menu extends Component{
-  state ={
-    products: products
+  constructor(){
+    super()
+    this.state ={
+      orders: [],
+            product: "",
+            price: ""
+  
+    }
+    this.clickItem = this.clickItem.bind(this)
   }
+clickItem(){
+  const itemName= this.props.product;
+  const itemPrice= this.props.price;
+  console.log(itemName, itemPrice);
+
+}
     
     render(){
         return  <div className="button-container">
-            {this.state.products.map(e=><button ><img src={e.img}/><p>{e.price}</p></button>)}
+            <button  onClick={this.clickItem}>
+              <img src={this.props.img}/>
+              <div>
+                <h6>{this.props.product}</h6>
+                <p>{this.props.price}</p>
+              </div>
+            </button>
         </div>
         
-        
-        
     }
-        
-        
-    
 }
 
 
--- src/assets/views/home.js
+++ src/assets/views/home.js
@@ -1,14 +1,25 @@
 import React, { Component } from 'react'
 //import ReactDOM from 'react-dom';
-import Navigation from '../components/Navigation'
-import Menu from '../components/Menu'
+import '../../index.css'
+import Logo from '../components/Logo'
+import { Link } from "react-router-dom";
+//import Navigation from '../components/Navigation'
+//import Menu from '../components/Menu'
+
 
 class Home extends Component{
 render(){
     return(
         <div>
-            <Navigation/>
-             <Menu/>
+                <Logo/>
+            <div className="divsHome">
+            <Link to="/Breakfast"><h1>Mesero</h1></Link>
+            
+            </div>
+            <div className="divsHome">
+            <Link to="/cocina"><h1>Cocina</h1></Link>
+            </div>
+            
         </div>
     )
 }
